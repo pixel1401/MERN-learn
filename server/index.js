6fc77051dc5023157cd8201c550b55121829cb28
pixsel1401 <pixsel1401@gmail.com>
2023-03-07 12:54:40 +0500

    backend finish

--- server/index.js
+++ server/index.js
@@ -10,9 +10,17 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import {register} from "./controllers/auth.js";
+import {createPost} from "./controllers/posts.js";
 
 import authRoutes from "./routes/auth.js";
+import usersRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+
+import {users , posts} from "./data/index.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -47,10 +55,13 @@ const upload = multer({ storage : storageConfig});
 
 // ROUTES WITH FILES
 app.post("/auth/register" , upload.single("picture") , register);
+app.post("/posts" , verifyToken , upload.single("picture") , createPost);
 
 
 // ROUTES
 app.use('/auth' , authRoutes);
+app.use('/users' , usersRoutes);
+app.use('/posts' , postsRoutes);
 
 
 // MONGOOSE SETUP
@@ -59,7 +70,11 @@ mongoose.connect(process.env.MONGO_URL , {
     useNewUrlParser: true,
   useUnifiedTopology: true
 }   ).then((res) => {
-    app.listen(PORT ,  ()=> console.log(`Server PORT : ${PORT}`)
-    )
+    app.listen(PORT ,  ()=> console.log(`Server PORT : ${PORT}`));
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+    
+
 }).catch((err)=> console.log(`${err} error`)
 )
---
+++ server/routes/users.js
@@ -0,0 +1,16 @@
+import express  from "express";
+import {verifyToken} from "../middleware/auth.js"
+import {getUsers , getUserFriends , addRemoveFriend} from "../controllers/users.js";
+
+
+const router = express.Router();
+
+
+// READ
+router.get('/:id' , verifyToken , getUsers);
+router.get("/:id.friends" , verifyToken , getUserFriends);
+
+// UPDATE
+router.patch('/:id/:friendId' , verifyToken , addRemoveFriend);
+
+export default router;
